857facf0c3044160fc4e1d2b69e58e07050a0e1c
monkypuzzle <paul.newsam@gmail.com>
2017-07-25 00:24:25 -0700

    Starts on sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :assignments
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   root 'assignments#index'
 end
