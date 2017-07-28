0ac44c83dc4610d4ed33ae84d9aca2cb3e3ece97
monkypuzzle <paul.newsam@gmail.com>
2017-07-27 19:05:14 -0700

    Adds routing for onboarding process

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :teachers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :assignments
-  resources :sections, only: [:index, :show]
+  resources :sections, only: [:index, :show] do
+    # get '/roster', to: '#roster'
+    resources :seats, only: [:index]
+  end
 
   get '/teachers/dashboard', to: 'teachers#dashboard'
 
