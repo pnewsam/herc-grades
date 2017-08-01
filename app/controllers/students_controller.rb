85d4b4e2ed7bd7fadb6bc8ae15fff5da55ed85c3
monkypuzzle <paul.newsam@gmail.com>
2017-08-01 13:30:11 -0700

    Adds student show page with grades

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,8 @@
 class StudentsController < ApplicationController
-  before_action :authenticate_student!
+  # before_action :authenticate_student!
 
-  
+  def show
+    @student = Student.find(params[:id])
+    @grades = @student.grades
+  end
 end
