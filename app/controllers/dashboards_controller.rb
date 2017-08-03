5b04adec579de34d11254cfc5cdaf4c734453ecd
monkypuzzle <paul.newsam@gmail.com>
2017-08-02 17:30:35 -0700

    Fixes extra seat creation bug in csv parser

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,8 +4,8 @@ class DashboardsController < ApplicationController
   def show
     @sections = Section.where(teacher_id: current_teacher.id).order(:period)
     @ungraded_assignments = Assignment.all.select{ |assignment| !assignment.fully_graded? }
-    @assignments = Assignment.all
-    @students = Student.all.decorate
+    @assignments = current_teacher.assignments
+    @students = current_teacher.students.decorate
   end
 
 end
