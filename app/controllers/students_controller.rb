8e5b9aa8666600f6c63b08ae5ae90ec50c461fde
monkypuzzle <paul.newsam@gmail.com>
2017-08-02 18:16:05 -0700

    Adds decorate to controllers

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,7 @@ class StudentsController < ApplicationController
   # before_action :authenticate_student!
 
   def show
-    @student = Student.find(params[:id])
+    @student = Student.find(params[:id]).decorate
     @grades = @student.grades
   end
 end
