487597c6293d271c96bab8e5399b1d593fc90355
monkypuzzle <paul.newsam@gmail.com>
2017-08-03 20:47:55 -0700

    Adds students#index

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,25 @@
 class StudentsController < ApplicationController
   before_action :authenticate_teacher!
 
-  def show
-    @student = Student.find(params[:id]).decorate
-    @grades = @student.grades
+  def index
   end
+
+  def show
+  end
+
+private
+  def students
+    @students ||= current_teacher.students.decorate
+  end
+  helper_method :students
+
+  def student
+    @student ||= current_teacher.students.find(params[:id]).decorate
+  end
+  helper_method :student
+
+  def grades
+    @grades ||= student.grades
+  end
+  helper_method :grades
 end
