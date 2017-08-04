666bcc67531b73bd922cd68afbd939f0e8772807
monkypuzzle <paul.newsam@gmail.com>
2017-08-04 14:54:02 -0700

    Updates seed with grading schemes, grade values

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,47 @@ Term.create(name: 'Spring')
 Term.create(name: 'Summer')
 
 School.create(name: 'No School')
-chula_vista = School.create(name: 'Chula Vista Middle School')
+
+fpn = GradingScheme.create(name: 'Full / Partial / No Credit')
+cn = GradingScheme.create(name: 'Credit / No Credit')
+lg = GradingScheme.create(name: 'Letter Grade')
+lgplus = GradingScheme.create(name: 'Letter Grade with +-')
+percentage = GradingScheme.create(name: 'Percentage')
+
+GradeValue.create(name: 'Full Credit', grading_scheme_id: fpn.id)
+GradeValue.create(name: 'Partial Credit', grading_scheme_id: fpn.id)
+GradeValue.create(name: 'No Credit', grading_scheme_id: fpn.id)
+
+GradeValue.create(name: 'Credit', grading_scheme_id: cn.id)
+GradeValue.create(name: 'No Credit', grading_scheme_id: cn.id)
+
+GradeValue.create(name: 'A', grading_scheme_id: lg.id)
+GradeValue.create(name: 'B', grading_scheme_id: lg.id)
+GradeValue.create(name: 'C', grading_scheme_id: lg.id)
+GradeValue.create(name: 'D', grading_scheme_id: lg.id)
+GradeValue.create(name: 'F', grading_scheme_id: lg.id)
+
+GradeValue.create(name: 'A+', grading_scheme_id: lgplus.id)
+GradeValue.create(name: 'A', grading_scheme_id: lgplus.id)
+GradeValue.create(name: 'A-', grading_scheme_id: lgplus.id)
+GradeValue.create(name: 'B+', grading_scheme_id: lgplus.id)
+GradeValue.create(name: 'B', grading_scheme_id: lgplus.id)
+GradeValue.create(name: 'B-', grading_scheme_id: lgplus.id)
+GradeValue.create(name: 'C+', grading_scheme_id: lgplus.id)
+GradeValue.create(name: 'C', grading_scheme_id: lgplus.id)
+GradeValue.create(name: 'C-', grading_scheme_id: lgplus.id)
+GradeValue.create(name: 'D+', grading_scheme_id: lgplus.id)
+GradeValue.create(name: 'D', grading_scheme_id: lgplus.id)
+GradeValue.create(name: 'D-', grading_scheme_id: lgplus.id)
+GradeValue.create(name: 'F', grading_scheme_id: lgplus.id)
+
+101.times do |i|
+  GradeValue.create(name: i.to_s, value: i, grading_scheme_id: percentage.id)
+end
 
 # # Development only:
 # ########################################
+chula_vista = School.create(name: 'Chula Vista Middle School')
 herc = Teacher.create(email: "herc@email.com", password: "password", school_id: chula_vista.id)
 
 gensci = Course.create(name: "General Science")
