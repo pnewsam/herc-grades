5fe170cf0f0a381a79a4a51a01238158a6365fe6
monkypuzzle <paul.newsam@gmail.com>
2017-08-11 23:17:55 -0700

    More progress on refactoring

--- app/assets/javascripts/seating_chart.js
+++ app/assets/javascripts/seating_chart.js
@@ -6,6 +6,7 @@
 // E .seating-chart__edit-button
 
 // B .student-roster
+// E .student-roster__record
 // E .student-roster__placeholder
 // E .student-roster__student
 
@@ -28,25 +29,21 @@
 // handleDrop()
 // handleDragover()
 
-
 $(document).on("turbolinks:load", function(){
 
   if (window.location.pathname.indexOf("sections") > 0) {
 
-    var section = new Section($(".seating-chart_container"), $("student-roster"))
+    var section = new Section($(".seating-chart"), $(".student-roster"), $(".assignment-list"))
+    
     $(window).resize(function(){
-      section.renderSeats();
+      section.resizeChart();
     });
     
-    $(".seating-chart_container").on("click",".seating-chart__delete",function(event){
-      rosterStudent.find('student-roster-name').show().attr("draggable","true").attr("ondragstart","dragstartHandler(event)");
-      rosterStudent.find('student-roster-placeholder').hide();
-      seat.empty();
-    });
-
   }
 
 });
+
+
 
 class Section {
 
@@ -57,21 +54,19 @@ class Section {
     this.fetchData();
 
     // DOM Nodes
-    
     this.seatingChart = seatingChart;
-    this.editButton;
-    this.assignComponents();
-    this.bindEvents();
-
+    this.editButton, this.seatNodes, this.studentNodes;
     this.assignmentList = assignmentList;
     this.studentRoster = studentRoster;
+    this.collectNodes();
+    this.bindEvents();
+
     
-    // Properties
-    this.containerWidth, this.seatSide;
     
     // State
     this.isEditable = false;
-  
+    this.seatingChartWidth, this.seatSideLength;
+
   }
 
   fetchData() {
@@ -84,23 +79,21 @@ class Section {
       dataType: 'json'
     })
     .done(function(response) {
-      assignModels(response);
-      if (response.seats.length > 0) {
-        that.renderSeats();
-        that.renderStudentRoster();
-      }
+      that.assignModels(response);
+      that.renderSeatingChart();
     });
   }
 
-  assignModels(response) {
-    that.section = response.section;
-    that.students = response.students;
-    that.seats = response.seats;
+  collectNodes() {
+    this.studentNodes = $(".seating-chart__student")
+    this.seatNodes = $(".seating-chart__seat")
+    this.editButton = this.seatingChart.find(".seating-chart__edit-button");
   }
 
-  assignComponents() {
-    this.editButton = this.seatingChart.find(".seating-chart__edit-button");
-
+  assignModels(response) {
+    this.section = response.section;
+    this.students = response.students;
+    this.seats = response.seats;
   }
 
   bindEvents() {
@@ -108,43 +101,16 @@ class Section {
   }
 
   updateSizing() {
-    let cW = this.container.width();
-    let sS = (cW / this.section.number_of_columns) - 10;
-    this.containerWidth = cW;
-    this.seatSide = sS;
-    this.seatingChart.height(sS * this.section.number_of_rows + this.section.number_of_rows * 10 + 5);
+    this.seatingChartWidth = this.seatingChart.width();
+    this.seatSideLength = (this.seatingChartWidth / this.section.number_of_columns) - 10;
+    this.seatingChart.height(this.seatSideLength * this.section.number_of_rows + this.section.number_of_rows * 15);
   }
-
-  renderSeats() {
-    this.seatingChart.empty();
-    this.updateSizing();
-    for (let i = 0; i < this.seats.length; i++) {
-      let seat = this.seats[i];
-      let student;
-      if (this.containerWidth < 550) {
-        findStudentBySeat(student.id).first_name[0];
-      }
-      else {
-        findStudentBySeat(student.id).first_name;
-      }
-      this.seatingChart.append(this.renderSeat(seat, student));
-    };
-  }
-
-  findStudentBySeat(seat) {
-    return (this.students.filter(function(student) {
-      return seat.student_id === student.id;
-    })[0]);
-  }
-
-  get
 
   renderRoster() {
     for (let i = 0; i < this.students.length; i++) {
       this.studentRoster.append(this.renderRosterName(this.students[i]));
     }
   }
-
 
   toggleEditable() {
     if (this.isEditable) {
@@ -157,68 +123,77 @@ class Section {
     }
   };
 
-
   renderSeatingChart() {
-    loopElsWithCallback(this.seats,this.appendStudent)
-  }
-  
-  renderSeat(seat,student) {
-    let studentName;
-    if (this.containerWidth < 550) { }
-    else { studentName = student.first_name };
-  }
-
-  translateSeat(seat, seatSide) {
-    let x = (seat.column_number * seatSide + seat.column_number * 10 + 5).toString() + 'px';
-    let y = (seat.row_number * seatSide + seat.row_number * 10 + 5).toString() + 'px';
-    seat.css("transform",`translateX(${x}) translateY(${y})`)
+    for (let i = 0; i < this.seats.length; i++) {
+      let seat = this.seats[i];
+      this.seatingChart.append(this.renderSeat(seat));
+      this.translateSeat(seat,this.seatSideLength);
+      this.findAndAppendStudent(seat);
+    }
+    this.resizeChart();
   }
 
+  findAndAppendStudent(seat,context) {
+    let student = this.students.filter(function(student) { return seat.student_id === student.id; })[0];
+    $(`#seat-${seat.id}`).append(this.renderStudent(student));
+  }
 
+  resizeChart() {
+    this.updateSizing();
+    this.collectNodes();
+    for (let i = 0; i < this.seatNodes.length; i++) {
+      let seatNode = $(this.seatNodes[i]);
+      let seat = this.seats[seatNode.attr("id").replace("seat-","") - 1]
+      seatNode.css("width",`${this.seatSideLength}px`).css("height",`${this.seatSideLength}px`);
+      this.translateSeat(seat,this.seatSideLength);
+    }
+    for (let j = 0; j < this.studentNodes.length; j++) {
+      let studentNode = $(this.studentNodes[j]);
+      studentNode.css("width",`${this.seatSideLength}px`).css("height",`${this.seatSideLength}px`)
+    }
+  }
+
+  translateSeat(seat, seatSideLength) {
+    let x = (seat.column_number * seatSideLength + seat.column_number * 10 + 5).toString() + 'px';
+    let y = (seat.row_number * seatSideLength + seat.row_number * 10 + 5).toString() + 'px';
+    $(`#seat-${seat.id}`).css("transform",`translateX(${x}) translateY(${y})`)
+  }
+
+  resizeEl(student) {
+    $(`#student-${student.id}`).css("width",`${seatSideLength}px`).css("length",`${seatSideLength}px`)
+  }
 
   // Render methods
 
-  renderSeatingChartSeat(seat) {
+  renderSeat(seat) {
     return (`
       <div id="seat-${seat.id}" class="seating-chart__seat">
       </div>
     `);
   };
 
-  renderSeatingChartStudent(student) {
+  renderStudent(student) {
     return (`
       <p id="student-${student.id}" class="seating-chart__student">
-      ${student.name}
+        ${student.first_name}
       </p>
     `);
   }
 
-  renderStudentRosterStudent(student) {
+  renderStudentRosterRecord(student) {
     return(`
-      <div id="roster-${student.id}" class="student-roster__student">
-        <p class="student-roster__placeholder tag is-medium">${student.first_name}</p>
+      <div id="roster-${student.id}" class="student-roster__record">
+        <p class="student-roster__placeholder tag is-medium">
+          ${this.responsiveStudentName(student,this.seatingChardWidth)}
+        </p>
       </div>
     `)
   }
 
   responsiveStudentName(student, seatingChartWidth) {
-    if (seatingChartWidth < 550) {
-      return ( student.first_name[0] )
-    }
-    else {
-      return ( student.first_name )
-    }
-  };
-
-
-  appendStudent(el, student) {
-    el.append(renderStudent(student));
+    if (seatingChartWidth < 550) { return ( student.first_name[0] ); }
+    else { return ( student.first_name ); }
   }
-
-  resizeChart() {
-
-  }
-
 
   makeDroppable(el) {
     el.attr("ondrop","handleDrop(e)").attr("ondragover","handleDragover(e)");
@@ -252,13 +227,3 @@ class Section {
   }
 
 }
-
-// Utility Functions
-
-var loopElsWithCallback = function(els, callbacks) {
-  for (let i = 0; i < els.length; i++) {
-    for (let j = 0; j < callbacks.length; j++) {
-      callbacks[j](els[i]);
-    }
-  }
-};
