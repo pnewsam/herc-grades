6057af09a6982ac9ad23371a98c8f6d3cf5d3680
monkypuzzle <paul.newsam@gmail.com>
2017-08-14 23:25:47 -0700

    Edits renders to incorporate isEditable status

--- app/assets/javascripts/sections/components/student.js
+++ app/assets/javascripts/sections/components/student.js
@@ -7,16 +7,28 @@ var Student = function(props) {
     lastName = props.last_name;
   }
 
-  function render() {
+  function render(isEditable) {
+    console.log(isEditable)
     var name;
     if (firstName) { name = firstName; }
     else { name = 'Empty' }
+    if (isEditable) {
+      console.log('if!')
+      return (`
+        <p id="student-${id}" class="seating-chart__student" draggable="true" ondragstart="dragAndDrop.handleDragstart(event)">
+          ${firstName}
+        </p>
+      `);
+    }
+    else {
+      console.log('else!')
       return (`
         <p id="student-${id}" class="seating-chart__student">
           ${firstName}
         </p>
       `);
     }
+  }
 
   // function responsiveName() {
   //   if (seatingChartWidth < 550) { return ( first_name.substr(0,3) + '.' ); }
