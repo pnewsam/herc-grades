986a51daa8c83af5e90593d38aca0f7b555c5f79
monkypuzzle <paul.newsam@gmail.com>
2017-08-14 23:22:09 -0700

    Fleshes out Student Roster js model and incorporates Placeholder model

--- app/assets/javascripts/sections/components/student_roster.js
+++ app/assets/javascripts/sections/components/student_roster.js
@@ -1,22 +1,45 @@
-var StudentRoster = function() {
+var StudentRoster = function(props) {
 
-  this.students;
-  this.isEditable;
+  var placeholders, container, students;
+  placeholders = [];
+  container = props.container;
 
-  function renderRoster() {
-    for (let i = 0; i < students.length; i++) {
-      $studentRoster.append(renderRosterName(students[i]));
+  init(props.placeholders);
+
+  function init(phs) {
+    for (let i = 0; i < phs.length; i++) {
+      ph = new Placeholder(phs[i]);
+      placeholders.push(ph);
     }
   }
 
-  function render(student) {
-    return(`
-      <div id="roster-${student.id}" class="student-roster__record">
-        <p class="student-roster__placeholder tag is-medium">
-          ${responsiveStudentName(student,seatingChardWidth)}
-        </p>
-      </div>
-    `);
+  function handleDrop(e) {
+    student = e.originalEvent.dataTransfer.getData("text")
+    $(e.target).append($(`#`)).prepend(`<a class="seating-chart__remove-student delete"></a>`);
   }
+
+  function makeDroppable(els) {
+    els.on("drop",function(event){
+      handleDrop(event);
+    });
+    els.on("dragover",function(event){
+      handleDragover(event);
+    });
+  }
+
+  function findPlaceholder(id) {
+    placeholders.filter(function(ph){ return ph.id === id })[0];
+  }
+
+  function render() {
+    $(container).html("").prepend(`<h2 class="title is-2">Change Seating</h2>`);
+    for (let i = 0; i < placeholders.length; i++) {
+      $(container).append(placeholders[i].render());
+    }
+  }
+
+  return({
+    render: render
+  });
 
 };
